1c85d3d89ddc57f424c2386cd4c16a9463b5f35f
Gab0 <gabriel_scf@hotmail.com>
2018-07-14 12:49:35 -0300

    refactor.

--- newsfilterScroller.py
+++ newsfilterScroller.py
@@ -10,12 +10,15 @@ from taskbarScroller import Scroller
 
 def parseAction(entity):
     T = "python /home/gabs/newsfilter/enterHyperlink.py -l %s"
-    return T % entity[1]
+    return T % entity[1]i
+
 
 def processData(data):
     for i, d in enumerate(data):
         data[i].append(i % 2)
     return data
+
+
 if __name__ == "__main__":
     scroller = Scroller(120, 0.3, 2, 7)
 
